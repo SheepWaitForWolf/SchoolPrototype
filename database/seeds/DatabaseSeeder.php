19f073c143fea77ee4f953651607ccac52af0a9d
SheepWaitForWolf <dglennie89@gmail.com>
2016-10-03 10:57:39 +0100

    UsersTableSeeder added
    new seeder class added and users table has been seeded to have first
    user details

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,9 @@
 <?php
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Seeder;
+
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +14,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+         Model::unguard();
+
+         $this->call(UsersTableSeeder::class);
     }
 }
---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->delete();
+        User::create(array(
+        'name'     => 'Chris Sevilleja',
+        'username' => 'sevilayha',
+        'email'    => 'chris@scotch.io',
+        'password' => Hash::make('awesome'),
+    ));
+    }
+}
