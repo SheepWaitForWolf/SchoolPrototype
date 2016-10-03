2b423f3deaefbcfc4aeda2d77204a4a7d77cec6b
SheepWaitForWolf <dglennie89@gmail.com>
2016-10-03 11:44:53 +0100

    Enrolment Model created
    Enrolment model based on absence model was generated and fillable fields
    assigned in model.

---
+++ app/Models/Enrolment.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Enrolment extends Model
+{
+    protected $table = 'enrolment';
+
+    protected $primaryKey = 'enrol_id';
+
+    public $timestamps = true;
+
+    protected $fillable = [
+    	'f_name',
+    	'l_name',
+    	'la',
+    	'school',
+    	'year_of_study',
+    	'academic_year'
+    ];
+
+    protected $guarded = [];
+}
