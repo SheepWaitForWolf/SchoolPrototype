56f66dddad01cde0f084df65d3b26c581bf144c9
SheepWaitForWolf <dglennie89@gmail.com>
2016-10-05 11:52:23 +0100

    Search-function-added
    Installed Angolia and added search function to controller

--- app/Http/Controllers/Api/SearchController.php
+++ app/Http/Controllers/Api/SearchController.php
@@ -11,6 +11,22 @@ class SearchController extends Controller
 {
     public function search(Request $request)
     {
-    	
+    	// First we define the error message we are going to show if no keywords
+        // existed or if no results found.
+        $error = ['error' => 'No results found, please try with different keywords.'];
+
+        // Making sure the user entered a keyword.
+        if($request->has('q')) {
+
+            // Using the Laravel Scout syntax to search the products table.
+            $posts = Product::search($request->get('q'))->get();
+
+            // If there are results return them, if none, return the error message.
+            return $posts->count() ? $posts : $error;
+
+        }
+
+        // Return the error message if no keywords existed
+        return $error;
     }
 }
